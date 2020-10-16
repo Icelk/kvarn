393d4fc7f65be35a5f90f3b41d00aa5aae213879
Iselk <62830417+Iselk@users.noreply.github.com>
2020-10-16 13:05:13 +0200

    Added build support for MacOS

---
+++ build_macOS.sh
@@ -0,0 +1,12 @@
+# Add osxcross toolchain to path
+export PATH="/mnt/m/RustOSX/osxcross/target/bin:$PATH"
+
+# Make libz-sys (git2-rs -> libgit2-sys -> libz-sys) build as a statically linked lib
+# This prevents the host zlib from being linked
+export LIBZ_SYS_STATIC=1
+
+# Use Clang for C/C++ builds
+export CC=o64-clang
+export CXX=o64-clang++
+
+cargo build --release --target x86_64-apple-darwin
