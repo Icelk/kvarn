aa28acd9497d103f7f55ea277cc2206c3801fa68
Iselk <62830417+Iselk@users.noreply.github.com>
2020-11-01 15:45:47 +0100

    Made extension crate more modular

--- arktis_extensions/src/lib.rs
+++ arktis_extensions/src/lib.rs
@@ -1,6 +1,5 @@
+#[allow(unused_imports)]
 use arktis::*;
-#[cfg(feature = "php")]
-use fastcgi_client::{Client, Params};
 
 #[cfg(feature = "php")]
 pub fn php() -> BoundExtension {
@@ -8,59 +7,129 @@ pub fn php() -> BoundExtension {
         extension_aliases: &[],
         file_extension_aliases: &["php"],
         ext: Extension::new(&|| {}, &|_, data| {
-            // Create connection to FastCGI server
-            let stream =
-                std::net::TcpStream::connect((std::net::Ipv4Addr::LOCALHOST, 6633)).unwrap();
-            let mut client = Client::new(stream, false);
-
-            let file_name = data
-                .path
-                .file_name()
-                .and_then(|os_str| os_str.to_str())
-                .unwrap_or("");
-            let mut file_path =
-                std::env::current_dir().expect("Failed to get current working directory!");
-            file_path.push(data.path);
-            let file_path = file_path.to_str().unwrap_or("");
-            let content_len = format!("{}", data.request.body().len());
-
-            let params = Params::with_predefine()
-                .set_request_method(data.request.method().as_str())
-                .set_script_name(file_name)
-                .set_script_filename(file_path)
-                .set_request_uri(data.request.uri().path_and_query().unwrap().as_str())
-                .set_document_uri(file_name)
-                .set_remote_addr("127.0.0.1")
-                .set_remote_port("12345")
-                .set_server_addr("127.0.0.1")
-                .set_server_port("80")
-                .set_server_name(arktis::SERVER_NAME)
-                .set_content_type("")
-                .set_content_length(&content_len);
-
+            // Content type will be HTML!
+            // Will be overriden by headers from PHP.
             *data.content_type = ContentType::Html;
-            // Fetch fastcgi server(php-fpm) response.
-            let output = match client.do_request(&params, &mut (&**data.request.body())) {
-                Ok(output) => match output.get_stdout() {
-                    Some(output) => output,
-                    None => {
-                        eprintln!("No stdout in response from FastCGI!");
-                        if let Some(err) = output
-                            .get_stderr()
-                            .and_then(|err| String::from_utf8(err).ok())
-                        {
-                            eprintln!("Stderr: {}", err)
-                        }
-                        return;
-                    }
-                },
+
+            let output = match cgi::fcgi_from_data(&data) {
+                Ok(vec) => vec,
                 Err(err) => {
-                    eprintln!("Failed to request from FastCGI server! Err: {}", err);
+                    eprintln!("{}", err);
                     return;
                 }
             };
-
             *data.response = ByteResponse::with_partial_header(output);
         }),
+    }
+}
+
+#[cfg(feature = "fastcgi-client")]
+pub mod cgi {
+    use super::*;
+    use fastcgi_client::{Client, Params};
+    use std::borrow::Cow;
+    use std::io;
+
+    pub enum FCGIError {
+        FailedToConnect(io::Error),
+        FailedToDoRequest(fastcgi_client::Error),
+        NoStdout,
+    }
+    pub fn connect_to_fcgi(
+        port: u16,
+        method: &str,
+        file_name: &str,
+        file_path: &str,
+        uri: &str,
+        body: &[u8],
+    ) -> Result<Vec<u8>, FCGIError> {
+        // Create connection to FastCGI server
+        let stream = match std::net::TcpStream::connect((std::net::Ipv4Addr::LOCALHOST, port)) {
+            Ok(stream) => stream,
+            Err(err) => return Err(FCGIError::FailedToConnect(err)),
+        };
+        let mut client = Client::new(stream, false);
+
+        let len = format!("{}", body.len());
+
+        let params = Params::with_predefine()
+            .set_request_method(method)
+            .set_script_name(file_name)
+            .set_script_filename(file_path)
+            .set_request_uri(uri)
+            .set_document_uri(file_name)
+            .set_remote_addr("127.0.0.1")
+            .set_remote_port("12345")
+            .set_server_addr("127.0.0.1")
+            .set_server_port("80")
+            .set_server_name(arktis::SERVER_NAME)
+            .set_content_type("")
+            .set_content_length(len.as_str());
+
+        match client.do_request(&params, &mut (&*body)) {
+            Ok(output) => match output.get_stdout() {
+                Some(output) => Ok(output),
+                None => Err(FCGIError::NoStdout),
+            },
+            Err(err) => Err(FCGIError::FailedToDoRequest(err)),
+        }
+    }
+    pub fn fcgi_from_data(data: &RequestData) -> Result<Vec<u8>, Cow<'static, str>> {
+        let file_name = match parse::format_file_name(data.path) {
+            Some(name) => name,
+            None => {
+                return Err(Cow::Borrowed("Error formatting file name!"));
+            }
+        };
+        let file_path = match parse::format_file_path(data.path) {
+            Ok(name) => name,
+            Err(_) => {
+                return Err(Cow::Borrowed("Getting working directory!"));
+            }
+        };
+        let file_path = match file_path.to_str() {
+            Some(path) => path,
+            None => {
+                return Err(Cow::Borrowed("Error formatting file path!"));
+            }
+        };
+
+        const PORT: u16 = 6633;
+
+        // Fetch fastcgi server response.
+        match connect_to_fcgi(
+            6633,
+            data.request.method().as_str(),
+            file_name,
+            file_path,
+            data.request.uri().path_and_query().unwrap().as_str(),
+            data.request.body(),
+        ) {
+            Ok(vec) => Ok(vec),
+            Err(err) => match err {
+                FCGIError::FailedToConnect(err) => Err(Cow::Owned(format!(
+                    "Failed to connect to FastCGI server on port {}. IO Err: {}",
+                    PORT, err
+                ))),
+                FCGIError::FailedToDoRequest(err) => Err(Cow::Owned(format!(
+                    "Failed to request from FastCGI server! Err: {}",
+                    err
+                ))),
+                FCGIError::NoStdout => Err(Cow::Borrowed("No stdout in response from FastCGI!")),
+            },
+        }
+    }
+}
+#[allow(dead_code)]
+pub mod parse {
+    use std::path::{Path, PathBuf};
+
+    pub fn format_file_name<P: AsRef<Path>>(path: &P) -> Option<&str> {
+        path.as_ref().file_name().and_then(|os_str| os_str.to_str())
+    }
+    pub fn format_file_path<P: AsRef<Path>>(path: &P) -> Result<PathBuf, std::io::Error> {
+        let mut file_path = std::env::current_dir()?;
+        file_path.push(path);
+        Ok(file_path)
     }
 }
