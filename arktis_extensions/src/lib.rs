98eb906d9260f48fb173b420c09f57249e0df7a4
Iselk <62830417+Iselk@users.noreply.github.com>
2020-11-07 18:09:32 +0100

    Added back all previous extension functionality to arktis_extensions crate

--- arktis_extensions/src/lib.rs
+++ arktis_extensions/src/lib.rs
@@ -1,6 +1,9 @@
 #[allow(unused_imports)]
 use arktis::*;
 
+#[cfg(feature = "templates")]
+pub use templates::templates;
+
 #[cfg(feature = "fastcgi-client")]
 pub mod cgi {
     use super::*;
@@ -136,12 +139,232 @@ pub fn php() -> BoundExtension {
     }
 }
 
+#[cfg(feature = "templates")]
+pub mod templates {
+    use super::*;
+    use std::path::PathBuf;
+    use std::sync::Arc;
+    use std::{collections::HashMap, str};
+
+    pub fn templates() -> BoundExtension {
+        BoundExtension {
+            extension_aliases: &["tmpl"],
+            file_extension_aliases: &[],
+            ext: Extension::new(&|| {}, &|_, data| {
+                *data.response = ByteResponse::without_header(handle_template(
+                    &data
+                        .args
+                        .iter()
+                        .map(|string| string.as_str())
+                        .collect::<Vec<&str>>(),
+                    data.response.body_from(data.content_start),
+                    data.storage,
+                ));
+            }),
+        }
+    }
+
+    pub fn handle_template(arguments: &[&str], file: &[u8], storage: &mut Storage) -> Vec<u8> {
+        // Get templates, from cache or file
+        let templates = read_templates(arguments.iter().skip(1).copied(), storage);
+
+        #[derive(Eq, PartialEq)]
+        enum Stage {
+            Text,
+            Placeholder,
+        };
+
+        let mut response = Vec::new();
+
+        let mut stage = Stage::Text;
+        let mut placeholder_start = 0;
+        let mut escaped = 0;
+        for (position, byte) in file.iter().enumerate() {
+            let is_escape = *byte == ESCAPE;
+
+            match stage {
+                // If in text stage, check for left bracket. Then set the variables for starting identifying the placeholder for template
+                // Push the current byte to response, if not start of placeholder
+                Stage::Text if (escaped == 0 && !is_escape) || escaped == 1 => {
+                    if *byte == L_SQ_BRACKET && escaped != 1 {
+                        placeholder_start = position;
+                        stage = Stage::Placeholder;
+                    } else {
+                        response.push(*byte);
+                    }
+                }
+                Stage::Placeholder if escaped != 1 => {
+                    // If placeholder closed
+                    if *byte == R_SQ_BRACKET {
+                        // Check if name is longer than empty
+                        if position.checked_sub(placeholder_start + 2).is_some() {
+                            // Good; we have UTF-8
+                            if let Ok(key) = str::from_utf8(&file[placeholder_start + 1..position])
+                            {
+                                // If it is a valid template?
+                                // Frick, we have to own the value for it to be borrow for Arc<String>, no &str here :(
+                                if let Some(template) = templates.get(&key.to_owned()) {
+                                    // Push template byte-slice to the response
+                                    for byte in &**template {
+                                        response.push(*byte);
+                                    }
+                                }
+                            }
+                        }
+                        // Set stage to accept new text
+                        stage = Stage::Text;
+                    }
+                }
+                // Else, it's a escaping character!
+                _ => {}
+            }
+
+            // Do we escape?
+            if is_escape {
+                escaped += 1;
+                if escaped == 2 {
+                    escaped = 0;
+                }
+            } else {
+                escaped = 0;
+            }
+        }
+        response
+    }
+    fn read_templates<'a, I: DoubleEndedIterator<Item = &'a str>>(
+        files: I,
+        storage: &mut Storage,
+    ) -> HashMap<Arc<String>, Arc<Vec<u8>>> {
+        let mut templates = HashMap::with_capacity(32);
+
+        for template in files.rev() {
+            if let Some(map) = read_templates_from_file(template, storage) {
+                for (key, value) in map.iter() {
+                    templates.insert(Arc::clone(key), Arc::clone(value));
+                }
+            }
+        }
+
+        templates
+    }
+    fn read_templates_from_file(
+        template_set: &str,
+        storage: &mut Storage,
+    ) -> Option<Arc<HashMap<Arc<String>, Arc<Vec<u8>>>>> {
+        if let Some(lock) = storage.try_template() {
+            if let Some(template) = lock.get(template_set) {
+                return Some(template);
+            }
+        }
+        let mut template_dir = PathBuf::from("templates");
+        template_dir.push(template_set);
+
+        match read_file(&template_dir, storage.get_fs()) {
+            Some(file) => {
+                let templates = Arc::new(extract_templates(&file[..]));
+                match storage.try_template() {
+                    Some(mut cache) => match cache.cache(template_set.to_owned(), templates) {
+                        Err(failed) => Some(failed),
+                        Ok(()) => Some(cache.get(template_set).unwrap()),
+                    },
+                    None => Some(templates),
+                }
+            }
+            None => None,
+        }
+    }
+    fn extract_templates(file: &[u8]) -> HashMap<Arc<String>, Arc<Vec<u8>>> {
+        let mut templates = HashMap::with_capacity(16);
+
+        let mut last_was_lf = true;
+        let mut escape = false;
+        let mut name_start = 0;
+        let mut name_end = 0usize;
+        let mut newline_size = 1;
+        for (position, byte) in file.iter().enumerate() {
+            // Ignore all CR characters
+            if *byte == CR {
+                newline_size = 2;
+                continue;
+            }
+            // If previous char was \, escape!
+            // New template, process previous!
+            if !escape && last_was_lf && *byte == L_SQ_BRACKET {
+                // If name is longer than empty
+                if name_end.checked_sub(name_start + 2).is_some() {
+                    // Check if we have a valid UTF-8 string
+                    if let Ok(name) = str::from_utf8(&file[name_start + 1..name_end - 1]) {
+                        // Check if value comes after newline, space, or right after. Then remove the CRLF/space from template value
+                        let add_after_name = if file.get(name_end + newline_size - 1) == Some(&LF) {
+                            newline_size
+                        } else {
+                            if file.get(name_end) == Some(&SPACE) {
+                                1
+                            } else {
+                                0
+                            }
+                        };
+                        // Then insert template; name we got from previous step, then bytes from where the previous template definition ended, then our current position, just before the start of the next template
+                        // Returns a byte-slice of the file
+                        templates.insert(
+                            Arc::new(name.to_owned()),
+                            Arc::new(
+                                file[name_end + add_after_name..position - newline_size].to_vec(),
+                            ),
+                        );
+                    }
+                }
+                // Set start of template name to now
+                name_start = position;
+            }
+            if *byte == R_SQ_BRACKET {
+                name_end = position + 1;
+            }
+
+            last_was_lf = *byte == LF;
+            escape = *byte == ESCAPE;
+        }
+        // Because we add the definitions in the start of the new one, check for last in the end of file
+        if name_end.checked_sub(name_start + 2).is_some() {
+            if let Ok(name) = str::from_utf8(&file[name_start + 1..name_end - 1]) {
+                // Check if value comes after newline, space, or right after. Then remove the CRLF/space from template value
+                let add_after_name = if file.get(name_end + newline_size - 1) == Some(&LF) {
+                    newline_size
+                } else {
+                    if file.get(name_end) == Some(&SPACE) {
+                        1
+                    } else {
+                        0
+                    }
+                };
+                templates.insert(
+                    Arc::new(name.to_owned()),
+                    Arc::new(file[name_end + add_after_name..file.len() - newline_size].to_vec()),
+                );
+            }
+        }
+        templates
+    }
+}
+
 pub fn download() -> BoundExtension {
     BoundExtension {
         extension_aliases: &["download"],
         file_extension_aliases: &[],
         ext: Extension::new(&|| {}, &|_, data| {
             *data.content_type = ContentType::Download;
+        }),
+    }
+}
+
+pub fn cache() -> BoundExtension {
+    BoundExtension {
+        extension_aliases: &["cache"],
+        file_extension_aliases: &[],
+        ext: Extension::new(&|| {}, &|_, data| {
+            if let Some(cache) = data.args.get(1).and_then(|arg| arg.parse().ok()) {
+                *data.cached = cache;
+            }
         }),
     }
 }
