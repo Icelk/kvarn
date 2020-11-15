3caaac4e8fba9b023a753be416cab6429421e34d
Icelk <62830417+Iselk@users.noreply.github.com>
2020-11-15 20:45:19 +0100

    New tool; chute, to convert MD to HTML and keep Kvarn extensions and include header and footer.

---
+++ kvarn_chute/src/lib.rs
@@ -0,0 +1,283 @@
+use pulldown_cmark::{html, Options, Parser};
+use std::{
+    fs,
+    io::{self, prelude::*, ErrorKind},
+    path,
+};
+
+pub mod chars {
+    /// Line feed
+    pub const LF: u8 = 10;
+    /// Carrage return
+    pub const CR: u8 = 13;
+    /// ` `
+    pub const SPACE: u8 = 32;
+    /// `!`
+    pub const BANG: u8 = 33;
+    /// `&`
+    pub const AMPERSAND: u8 = 38;
+    /// `>`
+    pub const PIPE: u8 = 62;
+    /// `[`
+    pub const L_SQ_BRACKET: u8 = 91;
+    /// `\`
+    pub const ESCAPE: u8 = 92;
+    /// `]`
+    pub const R_SQ_BRACKET: u8 = 93;
+}
+pub use chars::*;
+
+pub const EXTENSION_PREFIX: &[u8] = &[BANG, PIPE];
+pub const EXTENSION_AND: &[u8] = &[AMPERSAND, PIPE];
+
+pub fn parse_args(bytes: &[u8]) -> (Vec<Vec<String>>, usize) {
+    let mut segments = Vec::with_capacity(bytes.windows(2).fold(1, |acc, value| {
+        if value == EXTENSION_AND {
+            acc + 1
+        } else {
+            acc
+        }
+    }));
+    let mut args =
+        Vec::with_capacity(bytes.iter().fold(
+            1,
+            |acc, value| {
+                if *value == SPACE {
+                    acc + 1
+                } else {
+                    acc
+                }
+            },
+        ));
+    let mut last_break = 0;
+    let mut current_index = 0;
+    let mut last_was_ampersand = false;
+    for byte in bytes {
+        if *byte == LF {
+            if current_index - last_break > 1 {
+                let string = String::from_utf8(
+                    bytes[last_break..if bytes.get(current_index - 1) == Some(&CR) {
+                        current_index - 1
+                    } else {
+                        current_index
+                    }]
+                        .to_vec(),
+                );
+                if let Ok(string) = string {
+                    args.push(string);
+                }
+            }
+            break;
+        }
+        if *byte == SPACE && current_index - last_break > 1 {
+            let string = String::from_utf8(bytes[last_break..current_index].to_vec());
+            if let Ok(string) = string {
+                args.push(string);
+            }
+        }
+        if last_was_ampersand {
+            if *byte == PIPE {
+                // New segment!
+                segments.push(args.split_off(0));
+                // Can be directly after, since a space won't get added, len needs to be more than 0!
+                last_break = current_index + 1;
+            }
+            last_was_ampersand = false;
+        }
+        if *byte == AMPERSAND {
+            last_was_ampersand = true;
+        }
+        current_index += 1;
+        if *byte == SPACE {
+            last_break = current_index;
+        }
+    }
+    if !args.is_empty() {
+        segments.push(args);
+    }
+    // Plus one, since loop breaks before newline
+    (segments, current_index + 1)
+}
+pub fn extension_args(bytes: &[u8]) -> (Vec<Vec<String>>, usize) {
+    if bytes.starts_with(EXTENSION_PREFIX) {
+        let (vec, content_start) = parse_args(&bytes[EXTENSION_PREFIX.len()..]);
+        // Add EXTENSION_PREFIX.len(), since the fn started counting as though byte 0 was EXTENSION_PREFIX.len() actual byte.
+        (vec, content_start + EXTENSION_PREFIX.len())
+    } else {
+        (Vec::new(), 0)
+    }
+}
+
+pub fn exit_with_message(message: &'static str) -> ! {
+    eprintln!("{}", message);
+    std::process::exit(1)
+}
+
+pub fn open_file_with_metadata<P: AsRef<path::Path>>(path: P) -> (fs::File, fs::Metadata) {
+    match fs::File::open(path).and_then(|file| file.metadata().map(|metadata| (file, metadata))) {
+        Ok(file) => file,
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => {
+                exit_with_message("File not found, please check the entered path.")
+            }
+            ErrorKind::PermissionDenied => {
+                exit_with_message("You do not have permission to read the file specified.")
+            }
+            _ => exit_with_message("Encountered an unknown error reading the file specified."),
+        },
+    }
+}
+pub fn create_file<P: AsRef<path::Path>>(path: P) -> fs::File {
+    fn open<P: AsRef<path::Path>>(options: &fs::OpenOptions, path: P) -> fs::File {
+        match options
+            .open(&path)
+            // .and_then(|file| file.metadata().map(|metadata| (file, metadata)))
+        {
+            Ok(file) => file,
+            Err(err) => match err.kind() {
+                ErrorKind::AlreadyExists => {
+                    println!("Overriding existing file.");
+                    open(fs::OpenOptions::new().write(true).create(true).truncate(true), path)
+                }
+                ErrorKind::NotFound => {
+                    exit_with_message("File not found, please check the entered path.")
+                }
+                ErrorKind::PermissionDenied => {
+                    exit_with_message("You do not have permission to read the file specified.")
+                }
+                _ => exit_with_message("Encountered an unknown error reading the file specified."),
+            },
+        }
+    }
+    open(
+        fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .truncate(true),
+        path,
+    )
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+pub enum FileEnding {
+    LF,
+    CRLF,
+}
+impl FileEnding {
+    pub fn find(bytes: &[u8]) -> Option<Self> {
+        for byte_pair in bytes.windows(2) {
+            if byte_pair.get(1) == Some(&LF) {
+                match byte_pair.get(0) {
+                    Some(&CR) => return Some(Self::CRLF),
+                    Some(_) => return Some(Self::LF),
+                    None => {}
+                }
+            }
+        }
+        None
+    }
+    pub fn as_bytes(&self) -> &'static [u8] {
+        match self {
+            Self::LF => b"\n",
+            Self::CRLF => b"\r\n",
+        }
+    }
+}
+
+/// Will process the document at the given path.
+///
+/// If an Kvarn extension resides in the beginning of the header, make sure it has a newline in the end if only extension declaration; else parsing will fail.
+///
+/// # Errors
+/// Will only throw a error if writing to the output file failed. Else, it terminates the application.
+///
+/// # Panics
+/// If any unexpected event occurs, it will exit the application gracefully. This is not ment as a helper function.
+pub fn process_document<P: AsRef<path::Path>>(
+    path: P,
+    header: &[u8],
+    footer: &[u8],
+    ignored_extensions: &[&str],
+) -> io::Result<()> {
+    let path = path.as_ref();
+    if path.extension().and_then(|ext| ext.to_str()) != Some("md") {
+        println!("Specified file is not of type '.md' This conversion be a mistake and make a unexpected result.");
+    }
+    let (mut file, metadata) = open_file_with_metadata(&path);
+    let new_path = {
+        let mut path = path.to_owned();
+        path.set_extension("html");
+        path
+    };
+    let mut write_file = io::BufWriter::new(create_file(&new_path));
+
+    let mut buffer = Vec::with_capacity(metadata.len() as usize); // ToDo: Remove `as usize`
+    if let Err(_) = file.read_to_end(&mut buffer) {
+        exit_with_message("Encountered an error reading the contents of the file specified.")
+    }
+    let (mut extensions, header_content_starts) = extension_args(header);
+
+    let (file_extensions, file_content_start) = extension_args(&buffer[..]);
+    'extension_loop: for extension in file_extensions.into_iter() {
+        // If extension name is present
+        let extension_name = match extension.get(0) {
+            Some(e) => e.as_str(),
+            None => continue,
+        };
+        // Check for ignored extensions, and skip pushing it to main extensions if matched
+        for ignored in ignored_extensions {
+            if *extension_name == **ignored {
+                continue 'extension_loop;
+            }
+        }
+        // Push to main extension list
+        extensions.push(extension);
+    }
+    // Write all extensions
+    for (position, extension) in extensions.iter().enumerate() {
+        match position {
+            // Write !> in first iteration, &> in the rest!
+            0 => {
+                write_file.write(b"!>")?;
+                for arg in extension {
+                    write_file.write(b" ")?;
+                    write_file.write_all(arg.as_bytes())?;
+                }
+            }
+            _ => {
+                write_file.write(b" &>")?;
+                for arg in extension {
+                    write_file.write(b" ")?;
+                    write_file.write_all(arg.as_bytes())?;
+                }
+            }
+        }
+    }
+    // Write newline after extensions, ~depending on what file ending is used in the file~.
+    // Update, Pulldown-CMark only writes LF, even if input file is CRLF. I will also write LF then ¯\_(ツ)_/¯
+    write_file.write(
+        // FileEnding::find(&buffer[..])
+        //     .unwrap_or(FileEnding::LF)
+        //     .as_bytes(),
+        FileEnding::LF.as_bytes(),
+    )?;
+
+    // Write rest of header
+    write_file.write_all(&header[header_content_starts..])?;
+
+    // Parse CMark
+    let input = unsafe { std::str::from_utf8_unchecked(&buffer[file_content_start..]) };
+    let parser = Parser::new_ext(input, Options::all());
+
+    // Write HTML from specified file to output file (buffered for performance)
+    html::write_html(&mut write_file, parser)?;
+
+    // Writes footer
+    write_file.write_all(footer)?;
+    // Flushes the buffered writer
+    write_file.flush()?;
+
+    println!("Done converting CommonMark to HTML.");
+
+    Ok(())
+}
---
+++ kvarn_chute/src/main.rs
@@ -0,0 +1,23 @@
+use std::{env, io, io::Read};
+pub mod lib;
+
+fn main() {
+    println!("Starting Kvarn Markdown to Html converter.");
+    let mut args = env::args();
+    args.next();
+    let file = match args.next() {
+        Some(path) => path,
+        None => lib::exit_with_message("Please enter a filepath as the first argument."),
+    };
+    if let Err(_) = lib::process_document(
+        file,
+        b"!> tmpl standard markdown\n[head]\n[dependencies]\n[close-head]\n[navbar]\n<main style=\"text-align: center;\">",
+        b"</main>[footer]\n",
+        &["hide"]
+    ){
+        lib::exit_with_message("Failed to write to output file.");
+    }
+    println!("Press enter to close...");
+    let mut buffer = [0; 0];
+    let _ = io::stdin().read(&mut buffer);
+}
