e5d2f8fc87c414f7e93c8261ce6f8f4b8d0c8c4f
Icelk <main@icelk.dev>
2021-03-17 21:00:00 +0100

    Fixed countless compress issues and added preferences to each cached item

--- src/comprash.rs
+++ src/comprash.rs
@@ -1,34 +1,34 @@
 use crate::prelude::*;
-use bytes::Bytes;
+use bytes::{BufMut, Bytes};
 use http::Response;
-use std::{borrow::Borrow, hash::Hash};
-use tokio::sync::{Mutex, MutexGuard};
+use std::{borrow::Borrow, convert::TryInto, hash::Hash};
+// use tokio::sync::MutexGuard;
 
 pub type CachedResponse = Arc<Response<CachedCompression>>;
 
-pub struct Guard<'a, T> {
-    guard: MutexGuard<'a, Option<T>>,
-}
-impl<'a, T> Guard<'a, T> {
-    /// Will panic in runtime if the input `guard`s option is not `Some`
-    // If no compression features are used.
-    #[allow(dead_code)]
-    fn new(guard: MutexGuard<'a, Option<T>>) -> Self {
-        Self { guard }
-    }
-}
-impl<T> std::ops::Deref for Guard<'_, T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        self.guard.as_ref().unwrap()
-    }
-}
+// pub struct Guard<'a, T> {
+//     guard: MutexGuard<'a, Option<T>>,
+// }
+// impl<'a, T> Guard<'a, T> {
+//     /// Will panic in runtime if the input `guard`s option is not `Some`
+//     // If no compression features are used.
+//     #[allow(dead_code)]
+//     fn new(guard: MutexGuard<'a, Option<T>>) -> Self {
+//         Self { guard }
+//     }
+// }
+// impl<T> std::ops::Deref for Guard<'_, T> {
+//     type Target = T;
+//     fn deref(&self) -> &Self::Target {
+//         self.guard.as_ref().unwrap()
+//     }
+// }
 
-impl<T> std::ops::DerefMut for Guard<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.guard.as_mut().unwrap()
-    }
-}
+// impl<T> std::ops::DerefMut for Guard<'_, T> {
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         self.guard.as_mut().unwrap()
+//     }
+// }
 
 #[derive(Debug, PartialOrd, Ord)]
 pub enum UriKey<'a> {
@@ -86,21 +86,58 @@ impl Hash for UriKey<'_> {
     }
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
 pub struct CachedCompression {
     identity: http::Response<Bytes>,
-    gzip: Mutex<Option<http::Response<Bytes>>>,
-    br: Mutex<Option<http::Response<Bytes>>>,
+    gzip: Option<http::Response<Bytes>>,
+    br: Option<http::Response<Bytes>>,
+
+    compress: CompressPreference,
+    client_cache: ClientCachePreference,
 }
 impl CachedCompression {
-    pub(crate) fn new(identity: http::Response<Bytes>) -> Self {
+    pub(crate) fn new(
+        identity: http::Response<Bytes>,
+        compress: CompressPreference,
+        client_cache: ClientCachePreference,
+    ) -> Self {
         Self {
             identity,
-            gzip: Mutex::new(None),
-            br: Mutex::new(None),
+            gzip: None,
+            br: None,
+
+            compress,
+            client_cache,
         }
     }
     pub fn get_identity(&self) -> &http::Response<Bytes> {
         &self.identity
+    }
+
+    pub fn get_preferred(&self) -> &http::Response<Bytes> {
+        match self.compress {
+            CompressPreference::None => &self.identity,
+            CompressPreference::Full => {
+                #[cfg(all(feature = "gzip", feature = "br"))]
+                match self.br.is_some() && self.gzip.is_none() {
+                    true => return self.br.as_ref().unwrap(),
+                    false => self.get_gzip(),
+                }
+                #[cfg(all(feature = "gzip", not(feature = "br")))]
+                {
+                    self.get_gzip()
+                }
+                #[cfg(all(feature = "br", not(feature = "gzip")))]
+                {
+                    self.get_br()
+                }
+                #[cfg(not(any(feature = "gzip", feature = "br")))]
+                {
+                    &self.identity
+                }
+            }
+        }
     }
 
     #[allow(dead_code)]
@@ -113,35 +150,32 @@ impl CachedCompression {
             .version(response.version())
             .status(response.status());
         let mut map = response.headers().clone();
-        // http::header::OccupiedEntry::remove_entry_mult(self)
-        // http::header::Entry::
-        match map.entry("content-encoding") {
-            http::header::Entry::Vacant(slot) => {
-                slot.insert(compression);
-            }
-            http::header::Entry::Occupied(slot) => {
-                slot.remove_entry_mult();
-                map.insert("content-encoding", compression);
-            }
-        }
-
+        utility::replace_header(&mut map, "content-encoding", compression);
+        utility::replace_header(
+            &mut map,
+            "content-length",
+            // unwrap is ok, we know the formatted bytes from a number are (0-9) or `.`
+            http::HeaderValue::from_str(new_data.len().to_string().as_str()).unwrap(),
+        );
         *builder.headers_mut().unwrap() = map;
         builder.body(new_data).unwrap()
     }
 
-    #[cfg(features = "gzip")]
+    #[cfg(feature = "gzip")]
     /// Gets the gzip compressed version of [`CachedCompression::get_identity()`]
-    pub async fn get_gzip(&mut self) -> Guard<'_, http::Response<Bytes>> {
-        let mut lock = self.gzip.lock().await;
-        if lock.is_none() {
+    pub fn get_gzip(&self) -> &http::Response<Bytes> {
+        if self.gzip.is_none() {
             use std::io::Write;
             let bytes = self.identity.body().as_ref();
 
-            let mut buffer = bytes::BytesMut::with_capacity(bytes.len() / 2 + 64);
+            let mut buffer = bytes::BytesMut::with_capacity(bytes.len() / 2 + 64).writer();
+            // unsafe { buffer.set_len(buffer.capacity()) };
 
-            let mut c = flate2::write::GzEncoder::new(buffer.as_mut(), flate2::Compression::fast());
+            let mut c = flate2::write::GzEncoder::new(&mut buffer, flate2::Compression::fast());
             c.write(bytes).expect("Failed to compress using gzip!");
             c.finish().expect("Failed to compress using gzip!");
+
+            let buffer = buffer.into_inner();
             let buffer = buffer.freeze();
 
             let response = Self::clone_response_set_compression(
@@ -149,69 +183,83 @@ impl CachedCompression {
                 buffer,
                 http::HeaderValue::from_static("gzip"),
             );
-
-            *lock = Some(response);
+            // maybe shooting myself in the foot...
+            // but should be OK, since we only set it once, otherwise it's None.
+            unsafe {
+                (&mut *{ &self.gzip as *const _ as *mut Option<http::Response<Bytes>> }
+                    as &mut Option<http::Response<Bytes>>)
+                    .replace(response)
+            };
         }
-        Guard::new(lock)
+        self.gzip.as_ref().unwrap()
     }
-    #[cfg(features = "br")]
+    #[cfg(feature = "br")]
     /// Gets the Brotli compressed version of [`CachedCompression::get_identity()`]
-    pub async fn get_br(&mut self) -> Guard<'_, http::Response<Bytes>> {
-        let mut lock = self.gzip.lock().await;
-        if lock.is_none() {
+    pub fn get_br(&self) -> &http::Response<Bytes> {
+        if self.br.is_none() {
             use std::io::Write;
             let bytes = self.identity.body().as_ref();
 
-            let mut buffer = bytes::BytesMut::with_capacity(bytes.len() / 2 + 64);
+            let mut buffer = bytes::BytesMut::with_capacity(bytes.len() / 2 + 64).writer();
 
-            let mut c = brotli::CompressorWriter::new(buffer.as_mut(), 4096, 8, 21);
+            let mut c = brotli::CompressorWriter::new(&mut buffer, 4096, 8, 21);
             c.write(bytes).expect("Failed to compress using Brotli!");
             c.flush().expect("Failed to compress using Brotli!");
             c.into_inner();
+
+            let buffer = buffer.into_inner();
             let buffer = buffer.freeze();
 
             let response = Self::clone_response_set_compression(
                 self.get_identity(),
                 buffer,
-                http::HeaderValue::from_static("gzip"),
+                http::HeaderValue::from_static("br"),
             );
 
-            *lock = Some(response);
+            // maybe shooting myself in the foot...
+            // but should be OK, since we only set it once, otherwise it's None.
+            unsafe {
+                (&mut *{ &self.br as *const _ as *mut Option<http::Response<Bytes>> }
+                    as &mut Option<http::Response<Bytes>>)
+                    .replace(response)
+            };
         }
-        Guard::new(lock)
+        self.br.as_ref().unwrap()
     }
 }
-impl Debug for CachedCompression {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fn get_status(mutex: &Mutex<Option<http::Response<Bytes>>>) -> &'static str {
-            match mutex.try_lock() {
-                Err(_) => "[Busy]",
-                Ok(o) => match o.as_ref() {
-                    Some(_) => "Some",
-                    None => "None",
-                },
-            }
-        }
+// impl Debug for CachedCompression {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         fn get_status(mutex: &Mutex<Option<http::Response<Bytes>>>) -> &'static str {
+//             match mutex.try_lock() {
+//                 Err(_) => "[Busy]",
+//                 Ok(o) => match o.as_ref() {
+//                     Some(_) => "Some",
+//                     None => "None",
+//                 },
+//             }
+//         }
 
-        const RESPONSE: &str = "http::Response<Bytes>";
-        write!(
-            f,
-            "CachedCompression {{ identity: {}, gzip: {}({}), br: {}({})",
-            RESPONSE,
-            get_status(&self.gzip),
-            RESPONSE,
-            get_status(&self.br),
-            RESPONSE
-        )
-    }
-}
+//         const RESPONSE: &str = "http::Response<Bytes>";
+//         write!(
+//             f,
+//             "CachedCompression {{ identity: {}, gzip: {}({}), br: {}({})",
+//             RESPONSE,
+//             get_status(&self.gzip),
+//             RESPONSE,
+//             get_status(&self.br),
+//             RESPONSE
+//         )
+//     }
+// }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub enum CompressPreference {
     /// Will not auto-compress response body
     None,
     /// Will automatically compress and send compressed versions of the response
     Full,
 }
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub enum ServerCachePreference {
     /// Will not cache response
     None,
@@ -220,7 +268,22 @@ pub enum ServerCachePreference {
     /// Query does not matter and will be discarded
     Full,
 }
+impl ServerCachePreference {
+    pub fn cache(&self) -> bool {
+        match self {
+            Self::None => false,
+            Self::QueryMatters | Self::Full => true,
+        }
+    }
+    pub fn query_matters(&self) -> bool {
+        match self {
+            Self::None | Self::Full => false,
+            Self::QueryMatters => true,
+        }
+    }
+}
 /// Automatically add `cache-control` header to response
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub enum ClientCachePreference {
     /// Will not cache on client
     None,
@@ -263,7 +326,16 @@ impl<K: Eq + Hash> Cache<K> {
     {
         self.map.remove(key)
     }
-    pub fn cache(&mut self, key: K, value: http::Response<Bytes>) -> Option<CachedCompression> {
-        self.map.insert(key, CachedCompression::new(value))
+    pub fn cache(
+        &mut self,
+        key: K,
+        identity: http::Response<Bytes>,
+        compress: CompressPreference,
+        client_cache: ClientCachePreference,
+    ) -> Option<CachedCompression> {
+        self.map.insert(
+            key,
+            CachedCompression::new(identity, compress, client_cache),
+        )
     }
 }
