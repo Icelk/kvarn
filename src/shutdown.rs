318e4000090011e9a82c4388254ba24f53fb4a70
Icelk <main@icelk.dev>
2021-07-15 16:35:20 +0200

    Implemented handover. (for Unix) If you start a second instance of Kvarn, it'll signal the previous to shut down. This guarantees 100% uptime, at least on Unix.

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -74,6 +74,9 @@ pub struct Manager {
 
     #[cfg(feature = "graceful-shutdown")]
     channel: (WatchSender<()>, WatchReceiver<()>),
+
+    #[cfg(feature = "graceful-shutdown")]
+    handover_socket_path: Option<&'static str>,
 }
 impl Manager {
     /// Creates a new shutdown manager with the capacity of the list of wakers set to `_capacity`.
@@ -87,6 +90,8 @@ impl Manager {
                 wakers: WakerList::new(_capacity),
 
                 channel: watch_channel(()),
+
+                handover_socket_path: None,
             }
         }
         #[cfg(not(feature = "graceful-shutdown"))]
@@ -179,6 +184,8 @@ impl Manager {
     pub fn shutdown(&self) {
         info!("Initiating shutdown.");
         self.shutdown.store(true, Ordering::Release);
+        #[cfg(all(unix, not(target_os = "solaris"), not(target_os = "illumos")))]
+        std::fs::remove_file(self.handover_socket_path.unwrap_or(handover::SOCKET_PATH)).ok();
 
         if self.connections.load(Ordering::Acquire) == 0 {
             self.wakers.notify();
@@ -200,7 +207,183 @@ impl Manager {
             std::future::pending::<()>().await;
         }
     }
+    /// Initiates the handover from a old instance to the one currently running.
+    ///
+    /// This sends a `shutdown` message, waits for a reply, and then starts listening.
+    pub(crate) async fn initiate_handover(manager: &Arc<Self>, path: Option<&'static str>) {
+        #[cfg(all(unix, not(target_os = "solaris"), not(target_os = "illumos")))]
+        {
+            assert_eq!(
+                manager.handover_socket_path, None,
+                "Cannot call `initiate_handover` twice!"
+            );
+
+            let path = path.unwrap_or(handover::SOCKET_PATH);
+
+            // This is OK, we only write once, and accessing isn't important to be timely.
+            unsafe {
+                *(&manager.handover_socket_path as *const _ as *mut &'static str) = path;
+            }
+
+            match handover::send_to(b"shutdown", path).await.as_deref() {
+                handover::UnixResponse::Data(b"ok") | handover::UnixResponse::NotFound => {
+                    let manager = Arc::clone(manager);
+                    handover::start_at(
+                        move |data| match data {
+                            b"shutdown" => {
+                                manager.shutdown();
+                                (true, Vec::from("ok"))
+                            }
+                            _ => {
+                                let data = data.get(..128).unwrap_or(data);
+                                warn!(
+                                    "Got unexpected message on socket {:?}",
+                                    str::from_utf8(data).unwrap_or("BINARY")
+                                );
+                                (false, Vec::from("error"))
+                            }
+                        },
+                        path,
+                    )
+                    .await;
+                }
+                handover::UnixResponse::Data(data) => {
+                    error!(
+                        "Got unexpected reply from previous Kvarn instance: {:?}",
+                        str::from_utf8(&data)
+                    );
+                }
+                handover::UnixResponse::Error => {
+                    error!("Failed to message previous Kvarn instance. It might still be running.");
+                }
+            };
+        }
+    }
 }
+
+#[cfg(all(unix, not(target_os = "solaris"), not(target_os = "illumos")))]
+mod handover {
+    use crate::prelude::*;
+    use std::ops::Deref;
+    use tokio::net::{UnixListener, UnixStream};
+
+    /// Default message path.
+    pub(crate) const SOCKET_PATH: &str = "/tmp/kvarn.sock";
+
+    pub(crate) enum UnixResponse<T> {
+        /// The socket wasn't found, or had no listener.
+        NotFound,
+        /// An error occured in reading or writing.
+        Error,
+        /// Successful transmission.
+        Data(T),
+    }
+    impl<D: ?Sized, T: Deref<Target = D>> UnixResponse<T> {
+        /// Turns `&UnixResponse<T>` to `UnixResponse<&D>`
+        /// where `T: Deref<Target = D>`.
+        /// For example, `UnixResponse<Vec<u8>>.as_deref() == &UnixResponse<&[u8]>`
+        pub(crate) fn as_deref(&self) -> UnixResponse<&D> {
+            match self {
+                Self::NotFound => UnixResponse::NotFound,
+                Self::Error => UnixResponse::Error,
+                Self::Data(t) => UnixResponse::Data(t.deref()),
+            }
+        }
+    }
+
+    /// Sends `data` to a [`UnixListener`] at `path`.
+    pub(crate) async fn send_to(data: &[u8], path: impl AsRef<Path>) -> UnixResponse<Vec<u8>> {
+        let path = path.as_ref();
+        match UnixStream::connect(path).await {
+            Err(err) => match err.kind() {
+                io::ErrorKind::NotFound | io::ErrorKind::ConnectionRefused => {
+                    UnixResponse::NotFound
+                }
+                _ => {
+                    error!("Got error when trying to shut down previous instance of Kvarn: {:?}\nTrying to start server anyway.", err);
+                    UnixResponse::Error
+                }
+            },
+            Ok(mut connection) => {
+                if let Err(err) = connection.write_all(data).await {
+                    error!("Failed to send message! {:?}", err);
+                    UnixResponse::Error
+                } else {
+                    // Flushes the data.
+                    connection.shutdown().await.unwrap();
+
+                    let mut buf = Vec::new();
+                    if let Err(err) = connection.read_to_end(&mut buf).await {
+                        error!("Failed to receive message. {:?}", err);
+                        UnixResponse::Error
+                    } else {
+                        UnixResponse::Data(buf)
+                    }
+                }
+            }
+        }
+    }
+    /// Starts an [`UnixListener`] at `path` with `handler` receiving messages.
+    ///
+    /// The `handler` gets the data from the request, and should return whether to close the
+    /// listener and the data to send back.
+    pub(crate) async fn start_at(
+        handler: impl Fn(&[u8]) -> (bool, Vec<u8>) + Send + 'static,
+        path: impl AsRef<Path>,
+    ) {
+        let path = path.as_ref();
+        if tokio::fs::remove_file(path).await.is_ok() {
+            warn!("Removed old Kvarn socket.");
+        }
+        match UnixListener::bind(path) {
+            Err(err) => error!(
+                "Failed to listen on {:?}. Handover will not work! {:?}",
+                path, err
+            ),
+            Ok(listener) => {
+                tokio::spawn(async move {
+                    while let Ok((mut connection, _addr)) = listener.accept().await {
+                        let mut data = Vec::new();
+                        if timeout(
+                            std::time::Duration::from_millis(50),
+                            connection.read_to_end(&mut data),
+                        )
+                        .await
+                        .map(Result::ok)
+                        .ok()
+                        .flatten()
+                        .is_none()
+                        {
+                            warn!("Request didn't reach us in under 50 milliseconds.");
+                            continue;
+                        }
+                        let (close, data) = handler(&data);
+                        if timeout(
+                            std::time::Duration::from_millis(50),
+                            connection.write_all(&data),
+                        )
+                        .await
+                        .map(Result::ok)
+                        .ok()
+                        .flatten()
+                        .is_none()
+                        {
+                            warn!("We couldn't write response in under 50 milliseconds.");
+                            continue;
+                        }
+                        if let Err(err) = connection.shutdown().await{
+                            error!("Failed to flush content. {:?}", err);
+                        }
+                        if close {
+                            break;
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
+
 /// The result of [`AcceptManager::accept`].
 /// Can either be a new connection or a shutdown signal.
 /// The listener should be dropped right after the shutdown signal is received.
