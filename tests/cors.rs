323ab10740eeeccbe8cfc4321d4c2b66c157db41
Icelk <main@icelk.dev>
2021-07-22 12:04:26 +0200

    Added a bunch of CORS and extensions tests.

---
+++ tests/cors.rs
@@ -0,0 +1,287 @@
+use kvarn::prelude::*;
+use kvarn_testing::prelude::*;
+
+#[tokio::test]
+async fn default_deny() {
+    let server = ServerBuilder::default().run().await;
+    let response = server
+        .get("/")
+        .header("origin", "https://kvarn.org")
+        .send()
+        .await
+        .unwrap();
+
+    test_cors_response(response, false, line!());
+}
+#[tokio::test]
+async fn default_options() {
+    let server = ServerBuilder::default().run().await;
+    test_cors_options(
+        &server,
+        "/",
+        "https://doc.kvarn.org",
+        &[Method::POST],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+}
+
+fn get_extensions() -> Extensions {
+    let mut extensions = Extensions::new();
+    let cors = Cors::new()
+        .allow("/logo.svg", CorsAllowList::new().allow_all_origins())
+        .allow(
+            "/api/*",
+            CorsAllowList::new()
+                .add_origin("https://icelk.dev")
+                .add_origin("http://kvarn.org")
+                .add_origin("https://kvarn.org")
+                .add_method(Method::PUT)
+                .add_method(Method::DELETE),
+        )
+        .allow(
+            "/images/*",
+            CorsAllowList::new()
+                .add_origin("https://example.org")
+                .add_origin("https://foo.bar"),
+        )
+        .build();
+
+    extensions.add_cors(cors);
+    extensions
+}
+async fn test_cors_response(response: reqwest::Response, valid_expected: bool, line: u32) {
+    if valid_expected {
+        assert_eq!(
+            response.status(),
+            reqwest::StatusCode::NO_CONTENT,
+            "On line {} Response: {:#?}",
+            line,
+            response
+        );
+    } else {
+        assert_eq!(
+            response.status(),
+            reqwest::StatusCode::FORBIDDEN,
+            "On line {} Response: {:#?}",
+            line,
+            response
+        );
+        assert_eq!(
+            response.text().await.unwrap(),
+            "CORS request denied",
+            "On line {}",
+            line
+        );
+    }
+}
+async fn test_cors_options(
+    server: &Server,
+    path: impl AsRef<str>,
+    origin: impl AsRef<str>,
+    methods: &[Method],
+    headers: &[&str],
+    valid_expected: bool,
+    test_methods_and_headers: bool,
+    line: u32,
+) {
+    let mut request = server
+        .options(path.as_ref())
+        .header("origin", origin.as_ref());
+    if methods.len() > 0 {
+        let mut methods =
+            methods
+                .iter()
+                .map(Method::as_str)
+                .fold(String::new(), |mut s, method| {
+                    s.push_str(method);
+                    s.push_str(", ");
+                    s
+                });
+        methods.pop();
+        methods.pop();
+        request = request.header("access-control-request-method", methods);
+    }
+    if headers.len() > 0 {
+        let mut headers = headers.iter().fold(String::new(), |mut s, header| {
+            s.push_str(header);
+            s.push_str(", ");
+            s
+        });
+        headers.pop();
+        headers.pop();
+        request = request.header("access-control-request-headers", headers);
+    }
+
+    let response = request.send().await.unwrap();
+
+    if test_methods_and_headers {
+        let mut all_all_here = true;
+        if let Some(accepted_methods) = response
+            .headers()
+            .get("access-control-allow-methods")
+            .and_then(|h| h.to_str().ok())
+        {
+            let mut all_here = true;
+            for expected_method in methods {
+                if !accepted_methods.contains(expected_method.as_str()) {
+                    println!("NOT HERE!");
+                    all_here = false;
+                    break;
+                }
+            }
+            if !all_here {
+                all_all_here = false;
+            }
+        }
+        if let Some(accepted_headers) = response
+            .headers()
+            .get("access-control-allow-headers")
+            .and_then(|h| h.to_str().ok())
+        {
+                let mut all_here = true;
+                for expected_header in headers {
+                    if !accepted_headers.contains(expected_header) {
+                        all_here = false;
+                        break;
+                    }
+                }
+                println!("All headers here");
+                if !all_here {
+                    all_all_here = false;
+                }
+        }
+        assert_eq!(all_all_here, valid_expected, "On line {}", line);
+    } else {
+        test_cors_response(response, valid_expected, line).await;
+    }
+}
+#[tokio::test]
+async fn options() {
+    let server = ServerBuilder::from(get_extensions()).run().await;
+
+    test_cors_options(
+        &server,
+        "/logo.svg",
+        "ftp://foo.bar",
+        &[Method::GET],
+        &[],
+        true,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/api/test",
+        "ftp://foo.bar",
+        &[Method::PUT],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/api/test",
+        "http://icelk.dev",
+        &[],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/api/test",
+        "https://icelk.dev",
+        &[Method::GET, Method::PUT, Method::DELETE],
+        &[],
+        true,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/api/test",
+        "https://icelk.dev",
+        &[Method::GET, Method::PUT, Method::DELETE, Method::POST],
+        &[],
+        false,
+        true,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/api/test",
+        "https://icelk.dev",
+        &[Method::GET, Method::PUT, Method::DELETE],
+        &["content-type"],
+        false,
+        true,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/",
+        "https://icelk.dev",
+        &[Method::GET],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/images",
+        "https://example.org",
+        &[Method::GET],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/images/",
+        "https://example.org",
+        &[Method::GET],
+        &[],
+        true,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/images/my-funny-cat-pic.png",
+        "https://example.org",
+        &[Method::GET],
+        &[],
+        true,
+        false,
+        line!(),
+    )
+    .await;
+    test_cors_options(
+        &server,
+        "/images/my-funny-cat-pic.png",
+        "https://kvarn.org",
+        &[Method::GET],
+        &[],
+        false,
+        false,
+        line!(),
+    )
+    .await;
+}
---
+++ tests/extensions.rs
@@ -0,0 +1,235 @@
+use kvarn::prelude::*;
+use kvarn_testing::ServerBuilder;
+
+#[tokio::test]
+async fn prime_redirect() {
+    let extensions = get_extensions();
+
+    let server = ServerBuilder::new(extensions, host::Options::default())
+        .run()
+        .await;
+
+    let response = server.get("/").send().await.unwrap();
+    assert!(response.text().await.unwrap().contains("404 Not Found"));
+
+    let response = server
+        .get("/")
+        .header("user-agent", "curl")
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.text().await.unwrap(), "::1");
+
+    let response = server
+        .get("/")
+        .header(
+            "user-agent",
+            "Mozilla/5.0 (X11; Linux x86_64) Gecko/20100101 Firefox/78.0",
+        )
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.text().await.unwrap(), "<!DOCTYPE html>\n<html>\n<head>\n<title>Your IP address </title>\n</head>\n<body><h2>Your IP address is ::1</h2></body>\n</html>");
+
+    let response = server.get("/ip").send().await.unwrap();
+    assert_eq!(response.text().await.unwrap(), "::1");
+}
+
+#[tokio::test]
+async fn package_and_post() {
+    let run_validation = Arc::new(threading::atomic::AtomicBool::new(false));
+    let cloned_run_validation = run_validation.clone();
+
+    let mut extensions = get_extensions();
+    extensions.add_post(
+        post!(
+            _req,
+            _host,
+            _response,
+            _body,
+            _addr,
+            move |run_validation| {
+                run_validation.store(true, threading::Ordering::Release);
+            }
+        ),
+        extensions::Id::new(-42, "post test"),
+    );
+
+    let server = ServerBuilder::new(extensions, host::Options::default())
+        .run()
+        .await;
+
+    let response = server.get("/").send().await.unwrap();
+    assert_eq!(
+        response
+            .headers()
+            .get("content-security-policy")
+            .and_then(|header| header.to_str().ok()),
+        Some("default-src 'self'; style-src 'unsafe-inline' 'self'")
+    );
+
+    assert!(cloned_run_validation.load(threading::Ordering::Acquire));
+}
+
+#[tokio::test]
+async fn body() {
+    let length = 1024 * 16 + 7;
+
+    let server = ServerBuilder::default()
+        .with_extensions(|extensions| {
+            extensions.add_prepare_single(
+                "/api-1".to_string(),
+                prepare!(req, _host, path, addr {
+                    let body = req.body_mut().read_to_bytes().await.unwrap();
+                    let body = str::from_utf8(&body).unwrap();
+
+                    assert_eq!(body, "This is the full body.");
+                    assert_eq!(path.as_deref(), Some(Path::new("tests/public/api-1")));
+
+                    FatResponse::no_cache(Response::new(Bytes::from_static(b"OK")))
+                }),
+            );
+            extensions.add_prepare_single(
+                "/api-2".to_string(),
+                prepare!(req, _host, path, addr {
+                    let body = req.body_mut().read_to_bytes().await.unwrap();
+
+                    println!("Body len: {}", body.len());
+                    let expected = vec![chars::SPACE; length];
+
+                    assert_eq!(&body, &expected);
+
+                    FatResponse::no_cache(Response::new(Bytes::from_static(b"OK")))
+                }),
+            );
+            extensions.add_prepare_single(
+                "/api-3".to_string(),
+                prepare!(req, _host, path, addr {
+                    let body = req.body_mut().read_to_bytes().await.unwrap();
+                    let body = str::from_utf8(&body).unwrap();
+
+                    assert_eq!(body, "");
+
+                    FatResponse::no_cache(Response::new(Bytes::from_static(b"OK")))
+                }),
+            );
+        })
+        .run()
+        .await;
+    let response = server
+        .post("/api-1")
+        .header("content-type", "text/plain; encoding=utf-8")
+        .body("This is the full body.")
+        .send()
+        .await;
+
+    let body = vec![chars::SPACE; length];
+    let response = server
+        .post("/api-2")
+        .header("content-type", "application/octet-stream")
+        .body(body)
+        .send()
+        .await
+        .unwrap();
+    let response = server
+        .get("/api-3")
+        .header("content-type", "text/plain; encoding=utf-8")
+        .body("This is the full body.")
+        .send()
+        .await;
+}
+
+fn get_extensions() -> Extensions {
+    let mut extensions = Extensions::empty();
+
+    extensions.add_prime(prime!(request, host, addr {
+        assert_eq!(host.name, "localhost");
+        assert_eq!(addr.ip(), net::Ipv6Addr::LOCALHOST);
+
+        if request.uri().path() == "/" {
+            // This maps the Option<HeaderValue> to Option<Result<&str, _>> which the
+            // `.and_then(Result::ok)` makes Option<&str>, returning `Some` if the value is both `Ok` and `Some`.
+            // Could also be written as
+            // `.get("user-agent").and_then(|header| header.to_str().ok())`.
+            if let Some(ua) = request.headers().get("user-agent").map(HeaderValue::to_str).and_then(Result::ok) {
+                if ua.contains("curl") {
+                    Some(Uri::from_static("/ip"))
+                } else {
+                    Some(Uri::from_static("/index.html"))
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }), extensions::Id::new(16, "Redirect `/`"));
+
+    extensions.add_prepare_single(
+        "/ip".to_string(),
+        prepare!(_request, _host, _path, addr {
+            let ip = addr.ip().to_string();
+            let response = Response::new(Bytes::copy_from_slice(ip.as_bytes()));
+            FatResponse::no_cache(response)
+        }),
+    );
+    extensions.add_prepare_single(
+        "/index.html".to_string(),
+        prepare!(_request, _host, _path, addr {
+            let content = format!(
+                "!> simple-head Your IP address\n\
+                <h2>Your IP address is {}</h2>",
+                addr.ip()
+            );
+            let response = Response::new(Bytes::copy_from_slice(content.as_bytes()));
+            FatResponse::new(response, ServerCachePreference::None)
+        }),
+    );
+
+    extensions.add_present_internal(
+        "simple-head".to_string(),
+        present!(present_data {
+            let content = present_data.response().body();
+
+            let start = "\
+<!DOCTYPE html>
+<html>
+<head>
+<title>";
+            let middle = "\
+</title>
+</head>
+<body>";
+            let end = "\
+</body>
+</html>";
+            let title = present_data.args().iter().fold(String::new(), |mut acc, arg| {
+                acc.push_str(arg);
+                acc.push(' ');
+                acc
+            });
+
+            let bytes = build_bytes!(start.as_bytes(), title.as_bytes(), middle.as_bytes(), &content, end.as_bytes());
+            *present_data.response_mut().body_mut() = bytes;
+        }),
+    );
+    extensions.add_package(
+        package!(response, _request, _host {
+            response.headers_mut().insert("fun-header", HeaderValue::from_static("why not?"));
+            replace_header_static(response.headers_mut(), "content-security-policy", "default-src 'self'; style-src 'unsafe-inline' 'self'");
+        }),
+        extensions::Id::new(-1024, "add headers"),
+    );
+
+    extensions.add_post(
+        post!(_request, host, _response_pipe, body, addr {
+            if let Ok(mut body) = str::from_utf8(&body) {
+                body = body.get(0..512).unwrap_or(body);
+                println!("Sent {:?} to {} from {}", body, addr, host.name);
+            }
+        }),
+        extensions::Id::new(0, "Print sent data"),
+    );
+
+    extensions
+}
