bd06b3dd17f6b9265ce7112aa19f5dbea2afca1d
Icelk <main@icelk.dev>
2021-07-24 17:55:26 +0200

    Added more template tests

--- kvarn_extensions/tests/templates.rs
+++ kvarn_extensions/tests/templates.rs
@@ -50,7 +50,7 @@ async fn several_files() {
     }));
     let server = ServerBuilder::from(ext).path("./").run().await;
     let response = server.get("/").send().await.unwrap();
-    assert_eq!(response.text().await.unwrap(), "22[2]\n111\n");
+    assert_eq!(response.text().await.unwrap(), "\\2\\2[2]\n111\n");
 }
 
 #[cfg(feature = "templates")]
@@ -82,6 +82,53 @@ async fn escaping() {
     }));
     let server = ServerBuilder::from(ext).path("./").run().await;
     let response = server.get("/").send().await.unwrap();
-    assert_eq!(response.text().await.unwrap(), "[2]\n22[2]");
+    assert_eq!(response.text().await.unwrap(), "[2]\n\\2\\2[2]");
 }
 
+#[cfg(feature = "templates")]
+#[tokio::test]
+async fn spaces_before_template() {
+    let file = r"!> tmpl 3.txt
+   [spaces]";
+    let mut ext = kvarn_extensions::new();
+    ext.add_prepare_single("/index.html".to_string(), prepare!(_req, _host, _path, _addr {
+        FatResponse::no_cache(Response::new(Bytes::copy_from_slice(file.as_bytes())))
+    }));
+    let server = ServerBuilder::from(ext).path("./").run().await;
+    let response = server.get("/").send().await.unwrap();
+    assert_eq!(response.text().await.unwrap(), "   this contains spaces!");
+}
+
+#[cfg(feature = "templates")]
+#[tokio::test]
+async fn complex_data() {
+    let file = "\
+!> tmpl 3.txt
+this is complex
+[complex]
+data";
+    let mut ext = kvarn_extensions::new();
+    ext.add_prepare_single("/index.html".to_string(), prepare!(_req, _host, _path, _addr {
+        FatResponse::no_cache(Response::new(Bytes::copy_from_slice(file.as_bytes())))
+    }));
+    let server = ServerBuilder::from(ext).path("./").run().await;
+    let response = server.get("/").send().await.unwrap();
+    assert_eq!(response.text().await.unwrap(), "this is complex\noh <b>so</b> complex\ndata");
+}
+
+#[cfg(feature = "templates")]
+#[tokio::test]
+async fn tmpl_ignore() {
+    let file = "\
+!> tmpl 1.txt
+<---! tmpl-ignore -->
+[1!]
+data";
+    let mut ext = kvarn_extensions::new();
+    ext.add_prepare_single("/index.html".to_string(), prepare!(_req, _host, _path, _addr {
+        FatResponse::no_cache(Response::new(Bytes::copy_from_slice(file.as_bytes())))
+    }));
+    let server = ServerBuilder::from(ext).path("./").run().await;
+    let response = server.get("/").send().await.unwrap();
+    assert_eq!(response.text().await.unwrap(), "111\ndata");
+}
