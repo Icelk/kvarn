47ef9edc58a3aef65d33ef9fe14b47feace8c8b4
Icelk <main@icelk.dev>
2022-01-22 21:58:29 +0100

    Made PHP capturing more customizable. This allows WordPress sites to redirect all requests to WordPress.

--- extensions/src/php.rs
+++ extensions/src/php.rs
@@ -3,9 +3,22 @@ use crate::*;
 /// Redirects all requests where the [`Uri::path`] ends in `.php` to `connection`.
 ///
 /// Priority is `-8`.
-pub fn mount_php(extensions: &mut Extensions, connection: Connection) {
+///
+/// `capture_fn` can be used to pass other pages to PHP. Keep in mind the `Fn` is ran for every request, so to check if it matches several pages,
+/// create a hash map and try it there.
+///
+/// > Setting that to `Some(|req, _host| req.uri().path() == "/sitemap.xml")` can be useful if
+/// > you're running WordPress.
+pub fn mount_php(
+    extensions: &mut Extensions,
+    connection: Connection,
+    capture_fn: Option<impl Fn(&Request<application::Body>, &Host) -> bool + Send + Sync + 'static>,
+) {
     extensions.add_prepare_fn(
-        Box::new(|req, host| !host.options.disable_fs && req.uri().path().ends_with(".php")),
+        Box::new(move |req, host| {
+            (!host.options.disable_fs && req.uri().path().ends_with(".php"))
+                || (capture_fn.as_ref().map_or(false, |f| f(req, host)))
+        }),
         Box::new(move |req, host, path, addr| php(req, host, path, addr, connection)),
         extensions::Id::new(-8, "PHP"),
     );
