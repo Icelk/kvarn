2dc13386ed96098a73952f20d006ee2d7f9b8a0d
Icelk <main@icelk.dev>
2024-02-11 22:17:40 +0100

    Make auto cert more robust. Have had rare issues of self-signed certs being used on public websites. Most important fix is trying to check website ACME challenge availability several times, to account for transitory HTTP & DNS errors.

--- extensions/src/certificate.rs
+++ extensions/src/certificate.rs
@@ -11,13 +11,19 @@ use x509_parser::prelude::FromDer;
 
 static NOT_PUBLIC_ERROR: &str = "we aren't public-facing";
 
-/// `save_cert_pk`: Fn(certs, private_key)
+/// Attach automatic certificate generation to a host.
+/// `set_host_cert` is a callback, since we can't move `host` into a `'static` context, so you have
+/// to provide some method of calling [`Host::live_set_certificate`] on `host` (e.g. by saving a
+/// `Arc<host::Collection>` and getting the host from that).
+///
+/// `new_cert_immediately` should only be used when testing, since this extension reads the current
+/// cert and only updates it as needed.
 #[allow(clippy::too_many_arguments)]
 pub async fn mount<'a, F: Future + Send + 'a>(
     mut set_host_cert: impl FnMut(rustls::sign::CertifiedKey) -> F + Send + Sync + 'static,
     host: &Host,
     extensions: &mut Extensions,
-    new_cert_immedately: bool,
+    new_cert_immediately: bool,
     contact: impl IntoIterator<Item = String>,
     account_path: impl AsRef<Path>,
     cert_path: impl AsRef<Path>,
@@ -30,15 +36,22 @@ pub async fn mount<'a, F: Future + Send + 'a>(
     let cert_path = cert_path.as_ref().to_owned();
     let pk_path = pk_path.as_ref().to_owned();
 
-    let mut exp = if new_cert_immedately {
+    let mut exp = if new_cert_immediately {
         chrono::OffsetDateTime::now_utc()
     } else {
         let cert = host.certificate.read().unwrap();
         cert.as_ref()
             .and_then(|cert| {
-                get_expiration(cert.end_entity_cert().unwrap())
-                    // update cert every 60 days (Let's encrypt gives us 90 days)
-                    .map(|time| time - chrono::time::Duration::days(30))
+                get_expiration(cert.end_entity_cert().unwrap()).map(|(time, self_signed)| {
+                    // if self-signed, try to get proper
+                    if self_signed {
+                        info!("Detected self-signed cert on {domain}. Trying to get real certificate.");
+                        chrono::OffsetDateTime::now_utc()
+                    } else {
+                        // update cert every 60 days (Let's encrypt gives us 90 days; 90-60 = 30)
+                        time - chrono::time::Duration::days(30)
+                    }
+                })
             })
             .unwrap_or_else(chrono::OffsetDateTime::now_utc)
     };
@@ -76,6 +89,7 @@ pub async fn mount<'a, F: Future + Send + 'a>(
                 let tokens = t2.read().unwrap();
                 tokens.get(token).cloned()
             };
+            info!("Serving token on `/.well-known/acme-challenge/{token}`: {d:?}");
             let data = if let Some(d) = d {
                 d.into_bytes()
             } else {
@@ -158,14 +172,18 @@ pub async fn mount<'a, F: Future + Send + 'a>(
             .await
             .unwrap();
 
-            let (new_key, expiration, certs_pem, pk_pem) = match d {
+            let (new_key, expiration, certs_pem, pk_pem, self_signed) = match d {
                 Ok((new_key, expiration, (certs_pem, pk_pem))) => {
-                    (new_key, expiration, certs_pem, pk_pem)
+                    (new_key, expiration, certs_pem, pk_pem, false)
                 }
                 Err(small_acme::Error::Str(s)) if s == NOT_PUBLIC_ERROR => {
                     debug!("We're not public facing: don't renew certs");
-                    let Ok((og_key, key)) = generate_self_signed_cert(domain.clone()) else {
-                        return;
+                    let (og_key, key) = match generate_self_signed_cert(domain.clone()) {
+                        Ok(v) => v,
+                        Err(err) => {
+                            error!("Failed to create self-signed cert: {err}");
+                            return;
+                        }
                     };
                     info!(
                         "Using self-signed for {domain}. \
@@ -178,6 +196,7 @@ pub async fn mount<'a, F: Future + Send + 'a>(
                             + chrono::time::Duration::days(365 * 2000),
                         og_key.serialize_pem().unwrap(),
                         og_key.serialize_private_key_pem(),
+                        true,
                     )
                 }
                 Err(err) => {
@@ -196,20 +215,24 @@ pub async fn mount<'a, F: Future + Send + 'a>(
                         }
                     }
                     error!("Failed to renew / acquire TLS certificate: {err}");
-                    // retry in 5 minutes
-                    tokio::time::sleep(Duration::from_secs(300)).await;
+                    // retry in 60 minutes
+                    tokio::time::sleep(Duration::from_secs(60 * 60)).await;
                     continue;
                 }
             };
             account_failures = 0;
             set_host_cert(new_key).await;
-            // update cert every 60 days (Let's encrypt gives us 90 days)
-            exp = expiration - chrono::time::Duration::days(30);
-            if exp - chrono::OffsetDateTime::now_utc() < chrono::time::Duration::days(365 * 100) {
-                info!("Sleep until {exp} before renewing cert (which expires at {expiration}) on {domain}");
+
+            if self_signed {
+                exp = chrono::OffsetDateTime::now_utc() + chrono::time::Duration::days(7);
+                info!("Sleep until {exp} before retrying proper, not self-signed, certificate on {domain}");
+            } else {
+                // update cert every 60 days (Let's encrypt gives us 90 days)
+                exp = expiration - chrono::time::Duration::days(30);
+                info!("Sleep until {exp} before renewing cert (which expires at {expiration}) on {domain})");
             }
 
-            if !certs_pem.is_empty() {
+            if !certs_pem.is_empty() && !pk_pem.is_empty() && !self_signed {
                 if let Err(err) =
                     tokio::fs::create_dir_all(&cert_path.parent().unwrap_or_else(|| Path::new("/")))
                         .await
@@ -219,8 +242,6 @@ pub async fn mount<'a, F: Future + Send + 'a>(
                 if let Err(err) = tokio::fs::write(&cert_path, certs_pem).await {
                     error!("Failed to write new TLS certificate (chain): {err}");
                 }
-            }
-            if !pk_pem.is_empty() {
                 if let Err(err) =
                     tokio::fs::create_dir_all(&pk_path.parent().unwrap_or_else(|| Path::new("/")))
                         .await
@@ -296,20 +317,20 @@ pub fn get_cert(
 
     let authorizations = order.authorizations()?;
     let mut challenges = Vec::with_capacity(authorizations.len());
-    for authz in &authorizations {
-        match authz.status {
+    for auth in &authorizations {
+        match auth.status {
             AuthorizationStatus::Pending => {}
             AuthorizationStatus::Valid => continue,
             _ => return Err("unexpected authorization status!".into()),
         }
 
-        let challenge = authz
+        let challenge = auth
             .challenges
             .iter()
             .find(|c| c.r#type == ChallengeType::Http01)
             .ok_or("no http01 challenge found")?;
 
-        let Identifier::Dns(identifier) = &authz.identifier;
+        let Identifier::Dns(identifier) = &auth.identifier;
 
         set_token(
             &challenge.token,
@@ -322,13 +343,22 @@ pub fn get_cert(
     // Let the server know we're ready to accept the challenges.
 
     for (_, challenge) in &challenges {
-        let response = small_acme::ureq::get(&format!(
-            "http://{domain}/.well-known/acme-challenge/{}",
-            challenge.token
-        ))
-        .call()
-        .map_err(|_| NOT_PUBLIC_ERROR)?;
-        drop(response);
+        let mut tries = 0;
+        loop {
+            let url = format!(
+                "http://{domain}/.well-known/acme-challenge/{}",
+                challenge.token
+            );
+            if small_acme::ureq::get(&url).call().is_ok() {
+                break;
+            }
+            tries += 1;
+            if tries > 10 {
+                warn!("Tries 10 times to access {url}, but we don't seem to control that. Using self-signed!");
+                return Err(NOT_PUBLIC_ERROR.into());
+            }
+            std::thread::sleep(Duration::from_millis(1000));
+        }
         order.set_challenge_ready(&challenge.url)?;
     }
 
@@ -415,7 +445,8 @@ pub fn get_cert(
         }
     }
 
-    let expires = get_expiration(&certs[0]).ok_or("let's encrypt gave an invalid pem cert")?;
+    let (expires, _self_signed) =
+        get_expiration(&certs[0]).ok_or("let's encrypt gave an invalid pem cert")?;
 
     Ok((
         rustls::sign::CertifiedKey::new(
@@ -441,7 +472,12 @@ fn generate_self_signed_cert(
     Ok((key, rustls::sign::CertifiedKey::new(cert, pk)))
 }
 
-fn get_expiration(cert: &[u8]) -> Option<chrono::OffsetDateTime> {
+fn get_expiration(cert: &[u8]) -> Option<(chrono::OffsetDateTime, bool)> {
     let (_, cert) = x509_parser::certificate::X509Certificate::from_der(cert).ok()?;
-    Some(cert.validity().not_after.to_datetime())
+    let self_signed = cert.issuer().iter_organization().count() == 0
+        || cert
+            .issuer()
+            .iter_common_name()
+            .any(|n| n.as_str().map_or(false, |s| s.contains("rcgen")));
+    Some((cert.validity().not_after.to_datetime(), self_signed))
 }
