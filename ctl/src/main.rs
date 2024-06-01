07d9271fa425514be8e7ea027b31b1a92d26334a
Icelk <main@icelk.dev>
2024-06-01 22:59:14 +0200

    Fix cfg in kvarnctl for feature completion

--- ctl/src/main.rs
+++ ctl/src/main.rs
@@ -96,7 +96,7 @@ async fn main() {
                 .value_hint(ValueHint::Other),
         );
 
-    #[cfg(feature = "complete")]
+    #[cfg(feature = "completion")]
     {
         command = clap_autocomplete::add_subcommand(command);
     }
@@ -106,9 +106,9 @@ async fn main() {
         "COMMAND is required unless --wait or complete is used",
     );
 
-    #[cfg(feature = "complete")]
+    #[cfg(feature = "completion")]
     let mut shell_completion_command = command.clone();
-    #[cfg(feature = "complete")]
+    #[cfg(feature = "completion")]
     {
         shell_completion_command = shell_completion_command
             .subcommand(
@@ -216,7 +216,7 @@ async fn main() {
     }
     let matches = command.get_matches();
 
-    #[cfg(feature = "complete")]
+    #[cfg(feature = "completion")]
     if let Some(result) = clap_autocomplete::test_subcommand(&matches, shell_completion_command) {
         if let Err(err) = result {
             eprintln!("Insufficient permissions: {err}");
